aab9623310b3ae632d0033b65287d6ea8f7ccc78
sebastian <sebastian.burgstaller@gmail.com>
2017-04-26 01:57:15 -0700

    module setup an config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {
+  NgModule,
+  Component
+} from '@angular/core';
+
+import {
+  Router,
+  RouterModule,
+  Routes
+} from '@angular/router';
+
+import { RouteDef } from "./menu-bar/menu-bar.component";
+import { CompoundSearchComponent } from './compound-search/compound-search.component'
+import { CompoundDataComponent } from "./compound-data/compound-data.component";
+
+
+let examples: RouteDef[] = [
+  {label: 'Compound Search',          name: 'Root',          path: '',                component: CompoundSearchComponent},
+  {label: 'Compound Data',          name: 'compound_data',         path: 'compound_data/:qid',          component: CompoundDataComponent },
+
+];
+
+// export let routes: Routes = examples
+//   .map((example: RouteDef) => ({
+//     path: example.path, component: example.component, pathMatch: 'full'
+//   }));
+
+export const routes: Routes = [
+  { path: '', component: CompoundSearchComponent, pathMatch: 'full' },
+  { path: 'compound_data/:qid', component: CompoundDataComponent, pathMatch: 'full' }
+];
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="ui menu">
+      <div class="ui container">
+        <a href="#" class="header item">
+
+          <img src='../assets/logo-app.png' />
+        </a>
+        <div class="header item borderless">
+          <h1 class="ui header">
+            Repurpos.us: The Open and Expandable Drug Repurposing Portal
+          </h1>
+        </div>
+      </div>
+    </div>
+
+
+    <!--<div class="ui container">-->
+    <!--<div class="sixteen wide column">-->
+    <!--<menu-bar [items]="examples"></menu-bar>-->
+    <!---->
+    <!--</div>-->
+
+    <!--<div class="ui main container sixteen wide column">-->
+    <!--<router-outlet></router-outlet>-->
+    <!--</div>-->
+    <!--</div>-->
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span12">
+          <menu-bar [items]="examples"></menu-bar>
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+      <!--<div class="row-fluid">-->
+      <!--<div class="span12">-->
+      <!---->
+      <!--</div>-->
+      <!--</div>-->
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  examples: RouteDef[];
+
+  constructor(private router: Router) {
+    this.examples = examples; // store the outer examples
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,69 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { IntroComponent } from '/home/sebastian/ng-book-code/built_in_components/app/ts/app/intro_component';
+
+// import { DataTableModule, DataTable, DataTableResource } from 'angular-2-data-table';
+import { DataTableModule, DataTable, DataTableResource } from './table/index';
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+
+import { AppComponent, routes } from './app.component';
+import {MenuBarComponent, MenubarItemComponent} from './menu-bar/menu-bar.component';
+import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from "@angular/common";
+import {
+  CompoundSearchComponent, SearchResultComponent, SearchBox,
+  SearchResultTable, SearchResultService, Graph, GraphDataService, WDQService
+} from './compound-search/compound-search.component';
+
+import {cytoscape} from 'cytoscape';
+import {UnlessDirective} from "./compound-search/unless.directive";
+import {NgCytoscape} from "./compound-search/ng2-cytoscape";
+import {CompoundDataComponent} from './compound-data/compound-data.component';
+import {InteractionTableComponent, InteractionTableDataService} from './interaction-table/interaction-table.component';
+import { DiseaseDataComponent } from './disease-data/disease-data.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenubarItemComponent,
+    IntroComponent,
+    CompoundSearchComponent,
+    SearchResultComponent,
+    SearchResultTable,
+    SearchBox,
+    UnlessDirective,
+    NgCytoscape,
+    Graph,
+    CompoundDataComponent,
+    InteractionTableComponent,
+    DiseaseDataComponent,
+
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    DataTableModule,
+
+
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF,    useValue: '/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: SearchResultService, useClass: SearchResultService },
+    { provide: GraphDataService, useClass: GraphDataService },
+    { provide: WDQService, useClass: WDQService },
+    { provide: InteractionTableDataService, useClass: InteractionTableDataService },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
