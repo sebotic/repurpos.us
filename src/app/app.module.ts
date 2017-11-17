37f67057282cdf44bb003f20940eecc6290e748b
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2017-11-17 00:41:18 -0800

    added material design component imports and entryComponents specification for them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 
@@ -16,9 +16,9 @@ import { AppComponent, routes } from './app.component';
 import {MenuBarComponent, MenubarItemComponent} from './menu-bar/menu-bar.component';
 import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {
-  CompoundSearchComponent, SearchResultComponent, SearchBox,
-  SearchResultTable, SearchResultService, Graph, GraphDataService, WDQService
+  CompoundSearchComponent, SearchResultComponent, SearchBox, SearchResultService, WDQService
 } from './compound-search/compound-search.component';
+
 
 import {cytoscape} from 'cytoscape';
 import {UnlessDirective} from "./compound-search/unless.directive";
@@ -27,6 +27,47 @@ import {CompoundDataComponent} from './compound-data/compound-data.component';
 import {InteractionTableComponent, InteractionTableDataService} from './interaction-table/interaction-table.component';
 import { DiseaseDataComponent } from './disease-data/disease-data.component';
 import {CIDService, NglComponent} from './ngl/ngl.component';
+import { CompoundSearchOptionsComponent } from './compound-search/compound-search-options/compound-search-options.component';
+import { IndicationsGraphComponent, GraphDataService } from './compound-data/indications-graph/indications-graph.component';
+import { SearchResultTableComponent } from './compound-search/search-result-table/search-result-table.component';
+import {HttpClientModule, HttpClientXsrfModule, JsonpClientBackend, JsonpInterceptor} from "@angular/common/http";
+import {AuthComponent, AuthGuard, AuthService} from './auth/auth.component';
+import {DialogOverviewExample, DialogOverviewExampleDialog, EditItemComponent} from './edit-item/edit-item.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import {
+  // MatAutocompleteModule,
+  MatButtonModule,
+  // MatButtonToggleModule,
+  // MatCardModule,
+  // MatCheckboxModule,
+  // MatChipsModule,
+  // MatDatepickerModule,
+  MatDialogModule,
+  // MatExpansionModule,
+  // MatGridListModule,
+  // MatIconModule,
+  MatInputModule,
+  // MatListModule,
+  // MatMenuModule,
+  // MatNativeDateModule,
+  // MatPaginatorModule,
+  // MatProgressBarModule,
+  // MatProgressSpinnerModule,
+  // MatRadioModule,
+  // MatRippleModule,
+  // MatSelectModule,
+  // MatSidenavModule,
+  // MatSliderModule,
+  // MatSlideToggleModule,
+  // MatSnackBarModule,
+  // MatSortModule,
+  // MatTableModule,
+  // MatTabsModule,
+  // MatToolbarModule,
+  // MatTooltipModule,
+  // MatStepperModule,
+} from '@angular/material';
 
 
 @NgModule({
@@ -36,15 +77,22 @@ import {CIDService, NglComponent} from './ngl/ngl.component';
     MenubarItemComponent,
     CompoundSearchComponent,
     SearchResultComponent,
-    SearchResultTable,
     SearchBox,
     UnlessDirective,
     NgCytoscape,
-    Graph,
+    IndicationsGraphComponent,
     CompoundDataComponent,
     InteractionTableComponent,
     DiseaseDataComponent,
     NglComponent,
+    CompoundSearchOptionsComponent,
+    IndicationsGraphComponent,
+    SearchResultTableComponent,
+    AuthComponent,
+    AuthGuard,
+    EditItemComponent,
+    DialogOverviewExample,
+    DialogOverviewExampleDialog
 
 
   ],
@@ -54,9 +102,18 @@ import {CIDService, NglComponent} from './ngl/ngl.component';
     HttpModule,
     RouterModule.forRoot(routes),
     DataTableModule,
+    HttpClientModule,
 
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
 
+    FormsModule,
+    // MatNativeDateModule,
+    ReactiveFormsModule,
   ],
+
   providers: [
     { provide: APP_BASE_HREF,    useValue: '/' },
     { provide: LocationStrategy, useClass: HashLocationStrategy },
@@ -64,9 +121,15 @@ import {CIDService, NglComponent} from './ngl/ngl.component';
     { provide: GraphDataService, useClass: GraphDataService },
     { provide: WDQService, useClass: WDQService },
     { provide: InteractionTableDataService, useClass: InteractionTableDataService },
-    { provide: CIDService, useClass: CIDService}
+    { provide: CIDService, useClass: CIDService},
+    { provide: AuthService, useClass: AuthService},
 
   ],
+  entryComponents: [
+    DialogOverviewExample,
+    DialogOverviewExampleDialog,
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
