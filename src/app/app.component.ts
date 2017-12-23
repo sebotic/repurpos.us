acf44fbaaf64c230d7e71de01ba18cb97052a71a
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2017-12-23 01:00:54 -0800

    replaced WD login and email signup with real user account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { RouteDef } from "./menu-bar/menu-bar.component";
 import { CompoundSearchComponent } from './compound-search/compound-search.component'
 import { CompoundDataComponent } from "./compound-data/compound-data.component";
 import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from "@angular/common/http";
+import {isDefined} from "@angular/compiler/src/util";
 
 
 let routeDef: RouteDef[] = [
@@ -37,98 +38,151 @@ export class AppComponent implements OnInit{
     this.routeDef = routeDef;
   }
 
+  login(){
+    this.http.post('http://localhost:5000/auth/login', {"email": "sebastian.burgstaller@gmail.com", "password": "test"}, {
+        observe: 'response',
+        headers: new HttpHeaders()
+          .set('content-type', 'application/json')
+      }
+
+    ).subscribe((re) => {
+        let credentials = re.body;
+        console.log(credentials['auth_token']);
+        localStorage.setItem('auth_token', credentials['auth_token']);
+
+        console.log(JSON.stringify(re));
+        // console.log(re.status);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.error.message);
+        console.log(JSON.stringify(err));
+        console.log(err.status);
+      }
+    );
+  }
+
   ngOnInit(): void{
     console.log(this.document.location.pathname);
     console.log(this.document.location.href);
+
+    if (localStorage.getItem('auth_token') ){
+      this.http.get('http://localhost:5000/auth/status', {
+          observe: 'response',
+          headers: new HttpHeaders()
+            .set('content-type', 'application/json')
+            .set('Authorization', localStorage.getItem('auth_token'))
+        }
+
+      ).subscribe((re) => {
+          let credentials = re.body;
+          console.log(credentials['auth_token']);
+          // localStorage.setItem('auth_token', credentials['auth_token']);
+
+          console.log(JSON.stringify(re));
+          // console.log(re.status);
+        },
+        (err: HttpErrorResponse) => {
+          console.log('error executed');
+          // this.login();
+        }
+      );
+
+    } else {
+      // this.login();
+    }
+
+
+
+
 
     if (this.document.location.href.startsWith('https://repurpos') && this.document.location.href.includes('oauth_verifier')) {
       this.document.location.href = 'http://localhost:4200/?' + this.document.location.href.split('?')[1];
     }
     if (this.document.location.href.includes('oauth_verifier')) {
-      let url: string = 'http://localhost:5000/oauthcomplete?' + this.document.location.href.split('?')[1];
-      console.log('entered', url);
-
-      this.http.get(url, {
-          observe: 'response',
-          withCredentials: true,
-          headers: new HttpHeaders()
-            .set('Content-Type', 'application/json')
-        }
-
-      ).subscribe((res) => {
-        let bd = res.body;
-
-        console.log(JSON.stringify(bd));
-
-        this.http.get('http://localhost:5000/authenticated', {
-          observe: 'response',
-          withCredentials: true,
-          headers: new HttpHeaders()
-            .set('Content-Type', 'application/json')
-        }).subscribe((r) => {
-          let b = r.body;
-          console.log(JSON.stringify(b));
-
-        });
-
-        // return this.http.post('http://localhost:5000/write',{
-        //     observe: 'response',
-        //     params: new HttpParams()
-        //       .set('id', 'Q423111')
-        //       // .set('origin', 'http://localhost')
-        //       .set('origin', 'https://repurpos.us')
-        //       .set('action', 'wbeditentity')
-        //       .set('format', 'json')
-        //     ,
-        //
-        //     headers: new HttpHeaders()
-        //       .set('Authorization', this.auth_creds)
-        //       .set('content-type', 'application/json')
-        //       .set('charset', 'utf-8'),
-        //   }
-        //
-        // ).subscribe((re) => {
-        //     // do whatever with your response
-        //     // console.log(res['error']);
-        //
-        //     // console.log(res['success']);
-        //     // let bd2 = re.body;
-        //     //
-        //     console.log(JSON.stringify(re));
-        //     // console.log(re.status);
-        //   },
-        //   (err: HttpErrorResponse) => {
-        //     console.log(err.error.message);
-        //     console.log(JSON.stringify(err));
-        //     console.log(err.status);
-        //   }
-        // );
-
-          // this.http.get('https://www.wikidata.org/w/api.php', {
-          //     params: new HttpParams()
-          //       .set('ids', 'Q423111')
-          //       .set('action', 'wbgetentities')
-          //       .set('format', 'json')
-          //       .set('origin', '*')
-          //     ,
-          //
-          //     headers: new HttpHeaders()
-          //       // .set('content-type', 'application/x-www-form-urlencoded')
-          //       .set('charset', 'utf-8'),
-          //   }
-          //
-          // )
-          //
-          //   .subscribe((res) => {
-          //       // do whatever with your response
-          //       console.log(res);
-          //     },
-          //
-          //     err => {console.log(err);}
-          //
-          //   );
-
-      })
+      // let url: string = 'http://localhost:5000/oauthcomplete?' + this.document.location.href.split('?')[1];
+      // console.log('entered', url);
+      //
+      // this.http.get(url, {
+      //     observe: 'response',
+      //     withCredentials: true,
+      //     headers: new HttpHeaders()
+      //       .set('Content-Type', 'application/json')
+      //   }
+      //
+      // ).subscribe((res) => {
+      //   let bd = res.body;
+      //
+      //   console.log(JSON.stringify(bd));
+      //
+      //   this.http.get('http://localhost:5000/authenticated', {
+      //     observe: 'response',
+      //     withCredentials: true,
+      //     headers: new HttpHeaders()
+      //       .set('Content-Type', 'application/json')
+      //   }).subscribe((r) => {
+      //     let b = r.body;
+      //     console.log(JSON.stringify(b));
+      //
+      //   });
+      //
+      //   // return this.http.post('http://localhost:5000/write',{
+      //   //     observe: 'response',
+      //   //     params: new HttpParams()
+      //   //       .set('id', 'Q423111')
+      //   //       // .set('origin', 'http://localhost')
+      //   //       .set('origin', 'https://repurpos.us')
+      //   //       .set('action', 'wbeditentity')
+      //   //       .set('format', 'json')
+      //   //     ,
+      //   //
+      //   //     headers: new HttpHeaders()
+      //   //       .set('Authorization', this.auth_creds)
+      //   //       .set('content-type', 'application/json')
+      //   //       .set('charset', 'utf-8'),
+      //   //   }
+      //   //
+      //   // ).subscribe((re) => {
+      //   //     // do whatever with your response
+      //   //     // console.log(res['error']);
+      //   //
+      //   //     // console.log(res['success']);
+      //   //     // let bd2 = re.body;
+      //   //     //
+      //   //     console.log(JSON.stringify(re));
+      //   //     // console.log(re.status);
+      //   //   },
+      //   //   (err: HttpErrorResponse) => {
+      //   //     console.log(err.error.message);
+      //   //     console.log(JSON.stringify(err));
+      //   //     console.log(err.status);
+      //   //   }
+      //   // );
+      //
+      //     // this.http.get('https://www.wikidata.org/w/api.php', {
+      //     //     params: new HttpParams()
+      //     //       .set('ids', 'Q423111')
+      //     //       .set('action', 'wbgetentities')
+      //     //       .set('format', 'json')
+      //     //       .set('origin', '*')
+      //     //     ,
+      //     //
+      //     //     headers: new HttpHeaders()
+      //     //       // .set('content-type', 'application/x-www-form-urlencoded')
+      //     //       .set('charset', 'utf-8'),
+      //     //   }
+      //     //
+      //     // )
+      //     //
+      //     //   .subscribe((res) => {
+      //     //       // do whatever with your response
+      //     //       console.log(res);
+      //     //     },
+      //     //
+      //     //     err => {console.log(err);}
+      //     //
+      //     //   );
+      //
+      // })
 
     }
   }
