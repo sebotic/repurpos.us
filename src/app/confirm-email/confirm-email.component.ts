203be83a93226092ca54fd6f43c3c98b05273011
Tristan Jesse <tristanljesse@gmail.com>
2018-03-22 14:43:41 +0800

    Issue #22, resend confirm email link

--- src/app/confirm-email/confirm-email.component.ts
+++ src/app/confirm-email/confirm-email.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 import { HttpErrorResponse, HttpHeaders, HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
@@ -12,11 +13,18 @@ import { environment } from '../../environments/environment';
 export class ConfirmEmailComponent implements OnInit {
 	cid: string;
 	confirmResponse: string;
+	confirmFail: boolean = false;
+	confirmEmail: string;
+	confirmForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private http: HttpClient, @Inject(FormBuilder) fb: FormBuilder) {
   	route.params.subscribe(params => {
   		this.cid = params['cid'];
-  	})
+  	});
+
+  	this.confirmForm = fb.group({
+      email: new FormControl(this.confirmEmail, [Validators.required, Validators.email]),
+    });
   }
 
   ngOnInit() {
@@ -31,7 +39,48 @@ export class ConfirmEmailComponent implements OnInit {
   		}
   	).subscribe(res => {
   		this.confirmResponse = res.body['message'];
+  		if (res.body['status'] == 'fail') this.confirmFail = true;
+  		else this.confirmFail = false;
+  	},
+  	(err: HttpErrorResponse) => {
+  		console.log(err);
+  		this.confirmResponse = err.error.message;
+  		if (err.status != 501) this.confirmFail = true;
   	})
+  }
+
+  onSubmit(event) {
+  	this.http.post(environment.host_url + '/auth/confirm/link',
+      {"email": this.confirmEmail }, {
+        observe: 'response',
+        // withCredentials: true,
+        headers: new HttpHeaders()
+          .set('content-type', 'application/json')
+      }
+
+    ).subscribe((re) => {
+        let credentials = re.body;
+        if (credentials['status'] === 'success') {
+          //console.log(credentials['auth_token']);
+          //localStorage.setItem('auth_token', credentials['auth_token']);
+
+          this.confirmResponse = credentials['message'];
+          //location.reload();
+          // console.log(re.status);
+
+        } else if (credentials['status'] === 'fail') {
+          this.confirmResponse = credentials['message'];
+        }
+      },
+      (err: HttpErrorResponse) => {
+        this.confirmResponse = err.error.message;
+      }
+    )
+  }
+
+  getErrorMessage() {
+    return this.confirmForm.controls['email'].hasError('required') ? 'You must enter a value' :
+      this.confirmForm.controls['email'].hasError('email') ? 'Not a valid email' : '';
   }
 
 }
