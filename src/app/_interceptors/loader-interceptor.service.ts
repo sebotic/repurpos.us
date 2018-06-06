af7ef5cfdc383ee468f9c8efe9f39aa3db850fd8
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2018-06-06 18:03:11 +0200

    fixed search issue for browsers w/o local

--- src/app/_interceptors/loader-interceptor.service.ts
+++ src/app/_interceptors/loader-interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import { MatDialog } from '@angular/material';
 
