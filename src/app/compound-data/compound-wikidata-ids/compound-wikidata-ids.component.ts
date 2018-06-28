773ea37774246eb1bb6ce0e4417a7b479cf041a3
Laura Hughes <flaneuse@users.noreply.github.com>
2018-06-27 17:49:17 -0700

    minor cleanup to showing if not found

--- src/app/compound-data/compound-wikidata-ids/compound-wikidata-ids.component.ts
+++ src/app/compound-data/compound-wikidata-ids/compound-wikidata-ids.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
+import { GVKData, IntegrityData, InformaData } from '../../_models/index';
+
 @Component({
   selector: 'app-compound-wikidata-ids',
   templateUrl: './compound-wikidata-ids.component.html',
   styleUrls: ['./compound-wikidata-ids.component.css']
 })
+
 export class CompoundWikidataIdsComponent implements OnInit {
   @Input() qid: string;
   @Input() table_data: Array<Object> = [];
   @Input() idData: Array<Object> = [];
-  @Input() gvkData: Object = [];
-  @Input() informaData: Object = [];
-  @Input() integrityData: Object = [];
+  @Input() gvkData: GVKData;
+  @Input() informaData: InformaData;
+  @Input() integrityData: IntegrityData;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
