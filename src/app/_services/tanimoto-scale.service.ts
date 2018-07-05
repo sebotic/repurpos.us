690f7becfe34b1afd0cfc2e24f29a06d40048413
Laura Hughes <flaneuse@users.noreply.github.com>
2018-07-05 15:50:43 -0700

    adjust shades of red

--- src/app/_services/tanimoto-scale.service.ts
+++ src/app/_services/tanimoto-scale.service.ts
@@ -5,15 +5,17 @@ import * as Chroma from 'chroma-js';
 @Injectable()
 export class TanimotoScaleService {
 
-  public colorDomain: Array<string> = ['#ffffff', '#ffb6b0', '#f36664', '#c3152e'];
+  // public colorDomain: Array<string> = ['#ffffff', '#ffb6b0', '#f36664', '#c3152e'];
+  // brighter, more saturated red
+  public colorDomain: Array<string> = ['#ffffff', '#ffb6b0', '#f36664', '#FF5B67', '#DA0033'];
 
   constructor() { }
 
   getScale() {
     let domain = this.colorDomain;
 
-    let func = function(value: number, alpha: number = 1, colorDomain: string[] = domain) {
-      let scale = Chroma.scale(colorDomain).mode('lab');
+    let func = function(value: number, alpha: number = 1, colorDomain: string[] = domain, colorRange: number[] = [0.5, 1]) {
+      let scale = Chroma.scale(colorDomain).domain(colorRange).mode('lab');
 
       return (scale(value).alpha(alpha).css());
     }
