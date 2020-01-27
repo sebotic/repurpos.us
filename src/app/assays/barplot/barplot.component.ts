b60e41fdc1ab2c9be1c2385dfe90b14ff59c8804
Laura Hughes <flaneuse@users.noreply.github.com>
2020-01-27 10:06:39 -0800

    fix css encapsulation

--- src/app/assays/barplot/barplot.component.ts
+++ src/app/assays/barplot/barplot.component.ts
@@ -50,7 +50,7 @@ export class BarplotComponent implements OnInit, OnChanges {
         this.selected = this.yDomain;
       }
     })
-    
+
     this.createChart();
 
     this.drawChart();
@@ -131,7 +131,7 @@ export class BarplotComponent implements OnInit, OnChanges {
         .selectAll(".bar-group")
         .data(this.value_counts);
 
-      let barRect = bar_group.select(".assay-rect");
+      let barRect = bar_group.select(".barplot-rect");
       let barText = bar_group.select(".y-label");
       let checkboxes = bar_group.select(".checkbox");
       let checkmarks = bar_group.select(".checkmark");
@@ -146,7 +146,7 @@ export class BarplotComponent implements OnInit, OnChanges {
 
       let barEnter = barGroupEnter
         .append("rect")
-        .attr("class", "rect assay-rect")
+        .attr("class", "rect barplot-rect")
         .attr("height", this.y.bandwidth());
 
       let textEnter = barGroupEnter
